928a7d0cbb4c6ea2daba0dabb22b6d7b22e70ba1
Praveen <praveen.k@xnara.com>
2024-10-22 19:11:45 +0530

    added QnA

--- tests/QnA.spec.js
+++ tests/QnA.spec.js
@@ -15,3 +15,25 @@ test("Get all the text values from drop down", async ({page})=>{
         console.log(option)
     }
 })
+
+test("Student Registration Form", {tag: '@regression'}, async ({page})=>{
+    await page.goto("https://demoqa.com/automation-practice-form")
+    await page.locator("#firstName").fill("Test")
+    await page.locator("#lastName").fill("Testing")
+    await page.locator("#userEmail").fill("test@testing.com")    
+    await page.locator("[for='gender-radio-2']").setChecked(true)
+    await page.locator("#userNumber").pressSequentially("1234567890")
+    await page.locator(".subjects-auto-complete__value-container").click()
+    await page.locator(".subjects-auto-complete__value-container").pressSequentially("M")
+    await page.locator("#react-select-2-option-2").click()
+    await page.locator("[for='hobbies-checkbox-2']").check()
+    await page.locator("#currentAddress").fill("Hyderabad")
+    await page.getByText("Select State").click()
+    await page.locator("#react-select-3-option-0").click()
+    await page.getByText("Select City").click()
+    await page.locator("#react-select-4-option-0").click()
+    await page.locator("#submit").click()
+
+    await expect(page.locator("#example-modal-sizes-title-lg")).toContainText("Thanks for submitting the form")
+ 
+})
